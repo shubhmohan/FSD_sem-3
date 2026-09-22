e65839cf93b7da20b36345d8e24f178505d63fcc
Shubh <shubh903290@gmail.com>
2026-09-22 10:00:14 +0530

    fsd_exp

---
+++ exp-1a.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('greet', (name) => {
+    console.log(`Welcome, ${name}`);
+});
+
+myEmitter.on('exit', (code) => {
+    console.log(`Exit event received. Code: ${code}`);
+});
+
+myEmitter.emit('greet', 'B.Tech students');
+myEmitter.emit('exit', 0);
